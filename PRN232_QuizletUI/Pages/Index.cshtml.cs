67cce00d8d93eeea66fefa5c13b2d9321c9e5d23
S1nsoledad <hieupu2003@gmail.com>
2025-10-26 05:11:17 +0700

    homepage and search

--- PRN232_QuizletUI/Pages/Index.cshtml.cs
+++ PRN232_QuizletUI/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using PRN232_QuizletUI.Services;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System;
 
 namespace PRN232_QuizletUI.Pages
 {
@@ -16,19 +17,41 @@ namespace PRN232_QuizletUI.Pages
 
         public List<FlashcardSetViewModel> FlashcardSets { get; set; } = new();
 
-        public async Task OnGetAsync()
+        public int Page { get; set; }
+        public int TotalPages { get; set; }
+
+        public async Task OnGetAsync(string? search = "", int page = 1)
         {
-            FlashcardSets = await _api.GetAsync<List<FlashcardSetViewModel>>("https://localhost:7225/api/flashcardsets")
-                             ?? new List<FlashcardSetViewModel>();
+            string apiUrl = $"https://localhost:7225/api/flashcardsets?page={page}&pageSize=8";
+
+            if (!string.IsNullOrWhiteSpace(search))
+                apiUrl += $"&search={Uri.EscapeDataString(search)}";
+
+            var response = await _api.GetAsync<FlashcardSetResponse>(apiUrl);
+
+            FlashcardSets = response?.Data ?? new List<FlashcardSetViewModel>();
+            Page = response?.Page ?? 1;
+            TotalPages = response?.TotalPages ?? 1;
         }
+
 
         public class FlashcardSetViewModel
         {
-            public int SetID { get; set; }
+            public int SetId { get; set; }
             public string Title { get; set; }
             public string Description { get; set; }
-            public string CreatedBy { get; set; }
+            public string Author { get; set; }
             public int StudyCount { get; set; }
+            public DateTime CreatedAt { get; set; }
+        }
+
+        public class FlashcardSetResponse
+        {
+            public int Total { get; set; }
+            public int Page { get; set; }
+            public int PageSize { get; set; }
+            public int TotalPages { get; set; }
+            public List<FlashcardSetViewModel> Data { get; set; } = new();
         }
     }
 }
