1dc1787df01c261e424e7cf193e7f74679262308
Bui Duc Tuan <buiductuan.family@gmail.com>
2025-11-15 03:13:55 +0700

    Quiz fe

---
+++ PRN232_QuizletUI/Pages/Quiz/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN232_QuizletUI.Pages.Quiz
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
